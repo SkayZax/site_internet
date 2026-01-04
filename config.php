bc42dc15ef9822672a91567dcb36a3587d3d0e45
Mathis <mathis140500@gmail.com>
2026-01-04 16:29:21 +0100

    inutile

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Configuration de l'utilisateur et mot de passe (hashé)
-const APP_USER = 'mathis';
-const APP_PASS_HASH = '$2y$10$zxFXlQq4wnEIflyMs1XwXe7JNgqOVrV43JS6z7dcqu1PqoU3RXSBy';
-
-const APP_USER1 = 'nigga';
-const APP_PASS_HASH1 = '$2y$10$rLw8e25h13gGV.ckGE2XZOjctMcD8ajHUBFiQ16QNC.tjyKEhR3wC';
-
